8e577966fd8aa4bc7d9cd1b3d0b0e1d4d5d1f283
Bradley Hofbauer <bradley@framingnorth.com>
2021-06-01 15:48:48 -0500

    Added city and weather to the page

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 })
 
 function getWeather() {
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&APPID="+apiKey;
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&units=imperial&APPID="+apiKey;
 
   $.ajax(url,{success:function(data){
-    console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
   }})
 }
